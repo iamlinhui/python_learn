18eaaf14e389f9ec8c7e78e3a88d8ac03b98be49
lynn <lynn@promptness.cn>
2020-02-25 22:34:05 +0800

    U

--- day02/03.练习题.py
+++ day02/03.练习题.py
@@ -1,7 +1,35 @@
+# str1 = 'abcde'
+#
+# str2 = 'aebced'
+#
+# print(str1 & str2)
 
+year = 10
 
-str1 = 'abcde'
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    print("闰年")
+else:
+    print("平年")
 
-str2 = 'aebced'
+a = 3
+b = 2
+c = 1
 
-print(str1 & str2)
+if a > b:
+    t = a
+    a = b
+    b = t
+print("%d %d %d" % (a, b, c))
+# 231
+if a > c:
+    t = a
+    a = c
+    c = t
+print("%d %d %d" % (a, b, c))
+# 132
+if b > c:
+    t = b
+    b = c
+    c = t
+print("%d %d %d" % (a, b, c))
+# 123
